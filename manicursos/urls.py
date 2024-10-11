47a5071f55a4a17ce367bc0b8804efeffb5088c7
Rodrigo <rofen1@protonmail.com>
2024-10-11 15:49:39 -0300

    refactor: separar home em um aplicativo

--- manicursos/urls.py
+++ manicursos/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.footer.urls')),
+    path('', include('apps.home.urls')),
 ]
 
 
