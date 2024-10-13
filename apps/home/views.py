a2f339541b4af6c2f0b22f0e96b42d9be89accc4
Rodrigo <rofen1@protonmail.com>
2024-10-12 22:10:29 -0300

    feat: dinamizar times

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, redirect
 from ..footer.models import Contact, Information 
+from .models import Team
 
 def redirect_to_home(request):
     return redirect('home')
 
 def home(request):
-
     footerContacts = Contact.objects.all()
     footerInformations = Information.objects.all()
+    team = Team.objects.all()
 
     context = {
         'footerContacts': footerContacts,
         'footerInformations': footerInformations,
+        'team': team,
     }
     return render(request, 'home.html', context)
