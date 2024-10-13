3cba0dc0346cd6ee3a9e35fa947884c0f930bb8b
Rodrigo <rofen1@protonmail.com>
2024-10-12 21:55:32 -0300

    feat: redirecionar para a página home

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.redirect_to_home, name='redirect_to_home'),
+    path('home/', views.home, name='home'),
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from ..footer.models import Contact, Information 
+
+def redirect_to_home(request):
+    return redirect('home')
 
 def home(request):
 
