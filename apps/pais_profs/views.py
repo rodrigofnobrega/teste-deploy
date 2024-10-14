b544ce9e47c661b5c9c7afe056df83db62e336cc
VitorBurratto <vitorabd@gmail.com>
2024-10-14 14:38:45 -0300

    Feat: pagina pais e profs dinamizada
    - Pagina pais e profs dinamizada
    - Implementados filtros para seleção de tipos de materiais (colorir, didático e pais e professores).
    - Corrigidos pequenos bugs que afetavam a exibição dos elementos.

--- apps/pais_profs/views.py
+++ apps/pais_profs/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from ..footer.utils import getAllContacts, getAllInformations 
+from .models import TopDesc, DrawingAndCardAndKnow, CategoryP
 
 def parents(request):
-    return render(request, 'parents.html')
+    top_descs = TopDesc.objects.all()
+    drawings_and_cards = DrawingAndCardAndKnow.objects.all()
+    categories = CategoryP.objects.all()
+    
+    context = {
+        'top_descs': top_descs,
+        'drawings_and_cards': drawings_and_cards,
+        'categories': categories,
+    }
+    return render(request, 'parents.html', context)
